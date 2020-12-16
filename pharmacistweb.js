8f6235296c88543a04441547c9028902782aab7e
sukaynaDib <sukayna05@gmail.com>
2020-12-16 12:57:00 +0200

    add js

---
+++ pharmacistweb.js
@@ -0,0 +1,16 @@
+
+var studentName = prompt ("what is you name?"); 
+alert ("Hello  " + studentName);
+
+var university = prompt("which university you are attending?");
+alert ("great!")
+
+var uniNumber = prompt ("what is you university number?");{
+  
+    if (uniNumber > 1000 ) {
+        alert ("plz sign up");
+    
+    }else {
+        alert ("re write your correct university number")
+    }
+}
